718d594761f55284c94cc52f894b6b1bf64afb23
OrokanaKitsyne <orokana011899@gmail.com>
2026-04-29 00:18:15 +0300

    Delete MaxBot/parser.py

--- MaxBot/parser.py
+++
@@ -1,5 +0,0 @@
-﻿def get_courses():
-    return [
-        {"name": "Python", "description": "Курс по программированию на Python"},
-        {"name": "Веб-разработка", "description": "Курс по созданию сайтов"}
-    ]
